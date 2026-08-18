42a77c358bf09d8cfca624277457f1be1a21e55a
Joleen-Aweda <joleenaweda@protonmail.com>
2026-08-18 13:33:50 +0300

    Match bold emphasis to the original book

--- assets/source-bold.js
+++ assets/source-bold.js
@@ -1,32 +1,864 @@
 (() => {
-  const selector = "[data-bold-phrase]";
+  const sourceBoldMap = {
+  "pg008_n0011": {
+    "phrases": [
+      {
+        "text": "heri"
+      }
+    ]
+  },
+  "pg008_n0012": {
+    "phrases": [
+      {
+        "text": "bidii"
+      }
+    ]
+  },
+  "pg008_n0016": {
+    "phrases": [
+      {
+        "text": "chumba"
+      }
+    ]
+  },
+  "pg008_n0018": {
+    "phrases": [
+      {
+        "text": "mbwembwe"
+      }
+    ]
+  },
+  "pg008_n0022": {
+    "phrases": [
+      {
+        "text": "viranja"
+      }
+    ]
+  },
+  "pg014_n0023": {
+    "phrases": [
+      {
+        "text": "Mfano"
+      }
+    ]
+  },
+  "pg018_n0006": {
+    "phrases": [
+      {
+        "text": "nimuagize Pweza"
+      },
+      {
+        "text": "shwari"
+      }
+    ]
+  },
+  "pg018_n0019": {
+    "full": true
+  },
+  "pg020_n0005": {
+    "phrases": [
+      {
+        "text": "Mfano"
+      }
+    ]
+  },
+  "pg020_n0011": {
+    "phrases": [
+      {
+        "text": "Mfano"
+      }
+    ]
+  },
+  "pg020_n0098": {
+    "phrases": [
+      {
+        "text": "Mfano"
+      }
+    ]
+  },
+  "pg022_n0025": {
+    "phrases": [
+      {
+        "text": "honi"
+      }
+    ]
+  },
+  "pg023_n0004": {
+    "phrases": [
+      {
+        "text": "Dereva"
+      }
+    ]
+  },
+  "pg023_n0010": {
+    "phrases": [
+      {
+        "text": "abiria"
+      }
+    ]
+  },
+  "pg023_n0011": {
+    "phrases": [
+      {
+        "text": "kituo"
+      }
+    ]
+  },
+  "pg027_n0032": {
+    "phrases": [
+      {
+        "text": "Ndovu"
+      },
+      {
+        "text": "Mbogo"
+      }
+    ]
+  },
+  "pg027_n0035": {
+    "phrases": [
+      {
+        "text": "nyanya"
+      }
+    ]
+  },
+  "pg027_n0036": {
+    "phrases": [
+      {
+        "text": "gulioni"
+      }
+    ]
+  },
+  "pg027_n0037": {
+    "phrases": [
+      {
+        "text": "dagaa"
+      },
+      {
+        "text": "jamvi"
+      }
+    ]
+  },
+  "pg028_n0004": {
+    "phrases": [
+      {
+        "text": "motokaa"
+      }
+    ]
+  },
+  "pg028_n0005": {
+    "phrases": [
+      {
+        "text": "tumbili"
+      }
+    ]
+  },
+  "pg028_n0006": {
+    "phrases": [
+      {
+        "text": "ubwabwa"
+      }
+    ]
+  },
+  "pg028_n0009": {
+    "phrases": [
+      {
+        "text": "peremende"
+      }
+    ]
+  },
+  "pg029_n0010": {
+    "phrases": [
+      {
+        "text": "motokaa"
+      }
+    ]
+  },
+  "pg029_n0014": {
+    "phrases": [
+      {
+        "text": "ndovu"
+      }
+    ]
+  },
+  "pg029_n0019": {
+    "phrases": [
+      {
+        "text": "peremende"
+      }
+    ]
+  },
+  "pg029_n0023": {
+    "phrases": [
+      {
+        "text": "pesa"
+      },
+      {
+        "text": "fedha"
+      }
+    ]
+  },
+  "pg029_n0028": {
+    "phrases": [
+      {
+        "text": "peni"
+      }
+    ]
+  },
+  "pg030_n0039": {
+    "full": true
+  },
+  "pg032_n0025": {
+    "phrases": [
+      {
+        "text": "alipanda"
+      }
+    ]
+  },
+  "pg032_n0026": {
+    "phrases": [
+      {
+        "text": "alikwea"
+      }
+    ]
+  },
+  "pg032_n0030": {
+    "phrases": [
+      {
+        "text": "wakae"
+      }
+    ]
+  },
+  "pg032_n0033": {
+    "phrases": [
+      {
+        "text": "katani"
+      }
+    ]
+  },
+  "pg032_n0036": {
+    "phrases": [
+      {
+        "text": "sikukuu"
+      }
+    ]
+  },
+  "pg032_n0039": {
+    "phrases": [
+      {
+        "text": "Paka"
+      }
+    ]
+  },
+  "pg035_n0089": {
+    "phrases": [
+      {
+        "text": "dogo"
+      }
+    ]
+  },
+  "pg035_n0090": {
+    "phrases": [
+      {
+        "text": "mbali"
+      }
+    ]
+  },
+  "pg036_n0023": {
+    "phrases": [
+      {
+        "text": "mjinga"
+      }
+    ]
+  },
+  "pg036_n0026": {
+    "phrases": [
+      {
+        "text": "maskini"
+      }
+    ]
+  },
+  "pg036_n0027": {
+    "phrases": [
+      {
+        "text": "kuvunjwa"
+      }
+    ]
+  },
+  "pg036_n0028": {
+    "phrases": [
+      {
+        "text": "kupunguza"
+      }
+    ]
+  },
+  "pg036_n0029": {
+    "phrases": [
+      {
+        "text": "kung’oa"
+      }
+    ]
+  },
+  "pg038_n0004": {
+    "phrases": [
+      {
+        "text": "maovu"
+      }
+    ]
+  },
+  "pg038_n0015": {
+    "phrases": [
+      {
+        "text": "ridhaa"
+      }
+    ]
+  },
+  "pg039_n0002": {
+    "phrases": [
+      {
+        "text": "janga"
+      }
+    ]
+  },
+  "pg041_n0009": {
+    "phrases": [
+      {
+        "text": "ndondondo si chururu"
+      }
+    ]
+  },
+  "pg048_n0030": {
+    "phrases": [
+      {
+        "text": "afya"
+      }
+    ]
+  },
+  "pg049_n0005": {
+    "phrases": [
+      {
+        "text": "muuguzi"
+      }
+    ]
+  },
+  "pg050_n0025": {
+    "phrases": [
+      {
+        "text": "vilevile"
+      }
+    ]
+  },
+  "pg050_n0043": {
+    "phrases": [
+      {
+        "occurrence": 2,
+        "text": "mbele"
+      }
+    ]
+  },
+  "pg050_n0061": {
+    "phrases": [
+      {
+        "occurrence": 2,
+        "text": "Hata hivyo"
+      }
+    ]
+  },
+  "pg050_n0067": {
+    "phrases": [
+      {
+        "occurrence": 2,
+        "text": "kando"
+      }
+    ]
+  },
+  "pg055_n0005": {
+    "phrases": [
+      {
+        "text": "juu"
+      }
+    ]
+  },
+  "pg055_n0006": {
+    "phrases": [
+      {
+        "text": "kando"
+      }
+    ]
+  },
+  "pg055_n0010": {
+    "phrases": [
+      {
+        "text": "ndani"
+      }
+    ]
+  },
+  "pg055_n0011": {
+    "phrases": [
+      {
+        "text": "chini"
+      }
+    ]
+  },
+  "pg055_n0012": {
+    "phrases": [
+      {
+        "occurrence": 2,
+        "text": "ndani"
+      }
+    ]
+  },
+  "pg056_n0006": {
+    "phrases": [
+      {
+        "text": "nukta"
+      }
+    ]
+  },
+  "pg056_n0012": {
+    "phrases": [
+      {
+        "text": "mkato"
+      }
+    ]
+  },
+  "pg056_n0019": {
+    "phrases": [
+      {
+        "text": "mshangao"
+      }
+    ]
+  },
+  "pg056_n0025": {
+    "phrases": [
+      {
+        "text": "kiulizo"
+      }
+    ]
+  },
+  "pg057_n0003": {
+    "full": true
+  },
+  "pg058_n0026": {
+    "phrases": [
+      {
+        "text": "Shule"
+      }
+    ]
+  },
+  "pg058_n0027": {
+    "phrases": [
+      {
+        "text": "Rafiki"
+      }
+    ]
+  },
+  "pg059_n0006": {
+    "phrases": [
+      {
+        "text": "haki"
+      }
+    ]
+  },
+  "pg059_n0011": {
+    "phrases": [
+      {
+        "text": "mjinga"
+      }
+    ]
+  },
+  "pg064_n0011": {
+    "phrases": [
+      {
+        "occurrence": 12,
+        "text": "a"
+      },
+      {
+        "occurrence": 4,
+        "text": "z"
+      }
+    ]
+  },
+  "pg064_n0023": {
+    "phrases": [
+      {
+        "occurrence": 3,
+        "text": "o"
+      }
+    ]
+  },
+  "pg064_n0026": {
+    "phrases": [
+      {
+        "occurrence": 2,
+        "text": "t"
+      }
+    ]
+  },
+  "pg064_n0029": {
+    "phrases": [
+      {
+        "occurrence": 2,
+        "text": "k"
+      },
+      {
+        "text": "m"
+      }
+    ]
+  },
+  "pg064_n0037": {
+    "phrases": [
+      {
+        "occurrence": 3,
+        "text": "m"
+      }
+    ]
+  },
+  "pg064_n0040": {
+    "phrases": [
+      {
+        "text": "s"
+      }
+    ]
+  },
+  "pg064_n0048": {
+    "phrases": [
+      {
+        "text": "adabu"
+      }
+    ]
+  },
+  "pg065_n0002": {
+    "phrases": [
+      {
+        "text": "Aa"
+      }
+    ]
+  },
+  "pg065_n0004": {
+    "phrases": [
+      {
+        "text": "adabu"
+      }
+    ]
+  },
+  "pg065_n0009": {
+    "phrases": [
+      {
+        "text": "adabu"
+      }
+    ]
+  },
+  "pg065_n0012": {
+    "phrases": [
+      {
+        "occurrence": 7,
+        "text": "a"
+      }
+    ]
+  },
+  "pg065_n0018": {
+    "phrases": [
+      {
+        "occurrence": 2,
+        "text": "d"
+      }
+    ]
+  },
+  "pg065_n0021": {
+    "phrases": [
+      {
+        "occurrence": 14,
+        "text": "a"
+      },
+      {
+        "text": "d"
+      }
+    ]
+  },
+  "pg065_n0025": {
+    "phrases": [
+      {
+        "occurrence": 6,
+        "text": "a"
+      }
+    ]
+  },
+  "pg065_n0031": {
+    "phrases": [
+      {
+        "text": "b"
+      }
+    ]
+  },
+  "pg066_n0004": {
+    "phrases": [
+      {
+        "occurrence": 2,
+        "text": "u"
+      }
+    ]
+  },
+  "pg066_n0015": {
+    "phrases": [
+      {
+        "text": "adabu"
+      }
+    ]
+  },
+  "pg066_n0022": {
+    "phrases": [
+      {
+        "text": "mzigo"
+      }
+    ]
+  },
+  "pg066_n0026": {
+    "phrases": [
+      {
+        "text": "m"
+      }
+    ]
+  },
+  "pg066_n0030": {
+    "phrases": [
+      {
+        "occurrence": 2,
+        "text": "m"
+      }
+    ]
+  },
+  "pg066_n0034": {
+    "phrases": [
+      {
+        "text": "z"
+      }
+    ]
+  },
+  "pg066_n0044": {
+    "phrases": [
+      {
+        "text": "mzigo"
+      }
+    ]
+  },
+  "pg070_n0024": {
+    "phrases": [
+      {
+        "text": "kitindamimba"
+      }
+    ]
+  },
+  "pg071_n0017": {
+    "phrases": [
+      {
+        "text": "kikapu"
+      }
+    ]
+  },
+  "pg072_n0006": {
+    "phrases": [
+      {
+        "text": "njiapanda"
+      }
+    ]
+  },
+  "pg076_n0026": {
+    "phrases": [
+      {
+        "text": "mahafali"
+      }
+    ]
+  },
+  "pg077_n0002": {
+    "phrases": [
+      {
+        "text": "kasi"
+      }
+    ]
+  },
+  "pg077_n0003": {
+    "phrases": [
+      {
+        "text": "abiria"
+      }
+    ]
+  },
+  "pg078_n0002": {
+    "phrases": [
+      {
+        "text": "ajali"
+      }
+    ]
+  },
+  "pg078_n0008": {
+    "phrases": [
+      {
+        "text": "maisha"
+      }
+    ]
+  },
+  "pg079_n0028": {
+    "phrases": [
+      {
+        "text": "Mfano"
+      }
+    ]
+  },
+  "pg081_n0019": {
+    "phrases": [
+      {
+        "text": "mazingira"
+      }
+    ]
+  },
+  "pg081_n0024": {
+    "phrases": [
+      {
+        "text": "faida"
+      }
+    ]
+  },
+  "pg081_n0031": {
+    "phrases": [
+      {
+        "text": "Mmomonyoko"
+      }
+    ]
+  },
+  "pg082_n0009": {
+    "phrases": [
+      {
+        "text": "mazingira"
+      }
+    ]
+  },
+  "pg082_n0010": {
+    "phrases": [
+      {
+        "text": "faida"
+      }
+    ]
+  },
+  "pg082_n0011": {
+    "phrases": [
+      {
+        "text": "maradhi"
+      }
+    ]
+  },
+  "pg082_n0013": {
+    "phrases": [
+      {
+        "text": "mmomonyoko"
+      }
+    ]
+  },
+  "pg088_n0002": {
+    "phrases": [
+      {
+        "text": "madaha"
+      }
+    ]
+  },
+  "pg088_n0008": {
+    "phrases": [
+      {
+        "text": "mikogo"
+      }
+    ]
+  },
+  "pg066_n0041": {
+    "phrases": [
+      {
+        "text": "i"
+      },
+      {
+        "text": "g"
+      },
+      {
+        "text": "o"
+      }
+    ]
+  },
+  "pg066_n0057": {
+    "phrases": [
+      {
+        "text": "mbuga"
+      },
+      {
+        "text": "dereva"
+      },
+      {
+        "text": "kivuli"
+      },
+      {
+        "text": "ghala"
+      },
+      {
+        "text": "samani"
+      }
+    ]
+  }
+};
 
-  const applySourceBold = (element) => {
-    if (element.querySelector("strong[data-source-bold]")) return;
-
-    const phrase = element.dataset.boldPhrase;
-    const text = element.textContent;
-    const occurrence = Number(element.dataset.boldOccurrence || "1");
+  const findOccurrence = (text, phrase, occurrence = 1) => {
     let start = -1;
     let searchFrom = 0;
     for (let index = 0; index < occurrence; index += 1) {
       start = text.indexOf(phrase, searchFrom);
-      if (start < 0) return;
+      if (start < 0) return -1;
       searchFrom = start + phrase.length;
     }
+    return start;
+  };
 
-    const strong = document.createElement("strong");
-    strong.dataset.sourceBold = "true";
-    strong.textContent = phrase;
-    element.replaceChildren(
-      document.createTextNode(text.slice(0, start)),
-      strong,
-      document.createTextNode(text.slice(start + phrase.length)),
+  const applySourceBold = (element) => {
+    const id = element.dataset.id;
+    const mapped = sourceBoldMap[id];
+
+    if (mapped?.full) {
+      element.style.fontWeight = "700";
+      element.dataset.sourceBoldFull = "true";
+      return;
+    }
+
+    const phrases = mapped?.phrases || (
+      element.dataset.boldPhrase
+        ? [{
+            text: element.dataset.boldPhrase,
+            occurrence: Number(element.dataset.boldOccurrence || "1"),
+          }]
+        : []
     );
+    if (!phrases.length) return;
+    if (element.querySelectorAll("strong[data-source-bold]").length === phrases.length) return;
+
+    const text = element.textContent;
+    const ranges = phrases.map((entry) => {
+      const phrase = typeof entry === "string" ? entry : entry.text;
+      const occurrence = typeof entry === "string" ? 1 : (entry.occurrence || 1);
+      const start = findOccurrence(text, phrase, occurrence);
+      return { start, end: start + phrase.length, phrase };
+    }).filter(({ start }) => start >= 0).sort((a, b) => a.start - b.start);
+
+    if (!ranges.length) return;
+    const fragment = document.createDocumentFragment();
+    let cursor = 0;
+    ranges.forEach(({ start, end, phrase }) => {
+      if (start < cursor) return;
+      fragment.append(document.createTextNode(text.slice(cursor, start)));
+      const strong = document.createElement("strong");
+      strong.dataset.sourceBold = "true";
+      strong.textContent = phrase;
+      fragment.append(strong);
+      cursor = end;
+    });
+    fragment.append(document.createTextNode(text.slice(cursor)));
+    element.replaceChildren(fragment);
   };
 
   const applyAllSourceBold = () => {
-    document.querySelectorAll(selector).forEach(applySourceBold);
+    document.querySelectorAll("[data-id], [data-bold-phrase]").forEach(applySourceBold);
   };
 
   const observer = new MutationObserver(() => {
